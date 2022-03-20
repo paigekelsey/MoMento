7e2158d454eb634e37e0487373a97863e8f580a9
Kelsey Smith <pages.kelsey@gmail.com>
2022-03-19 23:42:55 -0400

    set up data models

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,7 @@ const App = () => {
     return (
         <Container maxWidth="lg">
           <AppBar className={classes.appBar} position="static" color="inherit">
-            <Typography className={classes.heading} variant="h2" align="center">Momento</Typography>
-            <img className={classes.image} src={memories} alt="icon" height="60" />
+            <img className={classes.image} src={memories} alt="icon" height="300" />
           </AppBar>
           <Grow in>
             <Container>
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+
 import App from './App';
 import './index.css';
 
-
-ReactDOM.render(
-  <Provider >
-    <App />
-  </Provider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
